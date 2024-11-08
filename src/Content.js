d54c590fff449ffba97fcd77bdfce5101db15f83
Saswat Levin <saswatlevin@gmail.com>
2024-11-08 23:51:00 +0530

    Added several buttons and onClick events <<Chapter 5 | 01tut>>

--- src/Content.js
+++ src/Content.js
@@ -4,12 +4,27 @@ const Content = () => {
         const index = Math.floor(Math.random() * 3);
         return names[index];
     };
+
+    const handleClick = () => {
+        console.log("A button was clicked.");
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked.`);
+    }
+
+    const handleClick3 = (myEvent) => {
+        console.log("The event is ", myEvent);
+    }
     
     return (
     <main>
-        <p>
+        <p onDoubleClick={handleClick}>
             Hello {handleNameChange()}!
         </p>
+        <button onClick={handleClick}>Button 1</button>
+        <button onClick={() => handleClick2('Dave')}>Button 2</button>
+        <button onClick={(myEvent) => handleClick3(myEvent)}>Button 3</button>
     </main>
   );
 };
