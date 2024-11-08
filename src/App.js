21e6ffe9fe35b93e7be0f95a7b8c141b3897928b
Saswat Levin <saswatlevin@gmail.com>
2024-11-08 21:53:57 +0530

    Changed App.js to a grocery list <<Chapter 3 | 01tut>>

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
   const name = "Dave";
-
-  const handleNameChange = () => {
-    const names = ["Ann", "Mary", "Dave"];
-    const index = Math.floor(Math.random() * 3);
-    return names[index];
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-          Hello, my name is {handleNameChange()}!
-        </p>
-      <p>{name}</p>
-      <p>{"string"}</p>
-      <p>{2.43}</p>
-      <p>{1}</p>
-      <p>{[1, 2, 3]}</p>
-      </header>
+      <Header/>
+      <Content/>
+      <Footer/>    
     </div>
     
   );
-}
+};
 
 export default App;
