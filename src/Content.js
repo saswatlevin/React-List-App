586e47964d92296c1b386e2fae435c3e4c3554fb
Saswat Levin <saswatlevin@gmail.com>
2024-11-09 22:41:40 +0530

    Added a counter to test state change <<Chapter 6 | 01tut>>

--- src/Content.js
+++ src/Content.js
@@ -1,30 +1,47 @@
+import { useState } from 'react';
+
 const Content = () => {
+    
+    // Here, name is the CURRENT STATE.
+    // Here, setName is any FUTURE STATE that we want to set.
+
+    // const is used since we don't want to change the state values directly.
+    // We only want to change the state values through the states themsleves.
+
+    // Another way to think about it is -> 
+    // name as GETTER (similar to getter function in object).   
+    // setName as SETTER (similar to setter function in object).
+    
+    const [name, setName] = useState('Dave');
+    
+    const [count, setCount] = useState(0);
+    
     const handleNameChange = () => {
         const names = ["Ann", "Mary", "Dave"];
         const index = Math.floor(Math.random() * 3);
-        return names[index];
+
+        // This allows the value of names[index] to be the FUTURE STATE.
+        setName(names[index]);
     };
 
     const handleClick = () => {
-        console.log("A button was clicked.");
-    }
+        setCount(count + 1);
+        console.log(count);
+    };
 
-    const handleClick2 = (name) => {
+    /*const handleClick2 = (name) => {
         console.log(`${name} was clicked.`);
-    }
+    }*/
 
     const handleClick3 = (myEvent) => {
         console.log("The event is ", myEvent);
-    }
+    };
     
     return (
     <main>
-        <p onDoubleClick={handleClick}>
-            Hello {handleNameChange()}!
-        </p>
-        <button onClick={handleClick}>Button 1</button>
-        <button onClick={() => handleClick2('Dave')}>Button 2</button>
-        <button onClick={(myEvent) => handleClick3(myEvent)}>Button 3</button>
+        <button onClick={handleNameChange}>Change Name</button>
+        <button onClick={handleClick}>Change Count</button>
+        <button onClick={(myEvent) => handleClick3(myEvent)}>Click to See Event</button>
     </main>
   );
 };
