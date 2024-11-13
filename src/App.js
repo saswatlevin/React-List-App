30b5df95181bc755b244de6d2b294c959ffcb2c4
Saswat Levin <saswatlevin@gmail.com>
2024-11-13 22:56:13 +0530

    Added all changes for App.js in Chapter 9 <<Chapter 9 | 01tut>>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Header from './Header';
+import SearchItem from './SearchItem';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import './index';
@@ -18,57 +20,104 @@ function App() {
     // setName as SETTER (similar to setter function in object).
     
     //const [name, setName] = useState('Dave');
-    const [items, setItems] = useState([
-      {
-          id: 1,
-          checked: false,
-          item: "item 1"
-      },
 
-      {
-          id: 2,
-          checked: false,
-          item: "item 2"
-      },
+    // Get the shopping list from local storage.
+    const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')));
 
-      {
-          id: 3,
-          checked: false,
-          item: "item 3"
-      }
-  ]);
+    const [newItem, setNewItem] = useState('');
+
+    const [search, setSearch] = useState('');
   
-  // Takes the list item key as an argument.
-  const handleCheck = (id) => {
-      console.log(`key: ${id}`);
-      // Map iterates over each list item
-      // The conditional statement in it finds the clicked element
-      // and negates its current checked value (true to false and vice-versa).
-      const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked }: item);
-      setItems(listItems);
-      localStorage.setItem('shoppingList', JSON.stringify(listItems));
-  };
-  
-  const handleDelete = (id) => {
-      // Create a list of listitems without the selected listitem
-      const listItems = items.filter((item) => item.id !== id);
-      // Set the listitems.
-      setItems(listItems);
-      if (listItems.length === 0){
+    // Add new items to the shopping list and store it in local storage.
+    const setAndSaveItems = (newItems) => {
+      // Set the new state.
+      setItems(newItems);
 
-         console.log("The list is empty"); 
-      }
-      // Put the listitems in local storage.
-      localStorage.setItem('shoppingList', JSON.stringify(listItems));
-  } 
-
+      // Save the list in local storage.
+      localStorage.setItem('shoppingList', JSON.stringify(newItems));
+    };
   
+    // Create a new item to be added to the shopping list
+    const addItem = (item) => {
+      // Get the id of the new item from the list and add 1 to it (the new item will be the last item on the list).
+      const id = items.length ? items[items.length - 1].id + 1: 1;
+    
+      // Create an object with the new item, the checked value and the id.
+      const myNewItem = {id, checked: false, item };
+    
+      // Add the item to the list using the spread operator.
+      const listItems = [...items, myNewItem];
+    
+      // Save the list in local storage.
+      setAndSaveItems(listItems);
+    };
+
+    // Takes the list item key as an argument.
+    const handleCheck = (id) => {
+        console.log(`key: ${id}`);
+        // Map iterates over each list item
+        // The conditional statement in it finds the clicked element
+        // and negates its current checked value (true to false and vice-versa).
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked }: item);
+        //setItems(listItems);
+        //localStorage.setItem('shoppingList', JSON.stringify(listItems));
+        setAndSaveItems(listItems);
+    };
+  
+    const handleDelete = (id) => {
+        // Create a list of listitems without the selected listitem
+        const listItems = items.filter((item) => item.id !== id);
+        
+        if (listItems.length === 0){
+
+          console.log("The list is empty"); 
+        }
+
+        // Set the listitems.
+        //setItems(listItems);
+        // Put the listitems in local storage.
+        //localStorage.setItem('shoppingList', JSON.stringify(listItems));
+        
+        setAndSaveItems(listItems);
+    }; 
+  
+    // Handles the submission of a new item in the list.
+    // Note that submssionEvent is a user defined event object. NOT built-in.
+    const handleSubmit = (submissionEvent) => {
+      // To prevent the page on reloading when the form is submitted
+      submissionEvent.preventDefault();
+    
+      // To prevent blank submissions
+      if (!newItem) return;
+
+      console.log("newItem received:", newItem);
+
+      // Add the new item to the list
+      addItem(newItem);
+
+      // Calling setNewItem()
+      // We set the new item to empty as we don't want to make the new item value persist on the input.
+      setNewItem('');
+    };
+
   return (
     <div className="App">
-      <Header/>
+      <Header title = 'Grocery List'/>
+      <AddItem
+        newItem = {newItem}
+        setNewItem = {setNewItem}
+        handleSubmit = {handleSubmit}
+      />
+      <SearchItem 
+        search = {search}
+        setSearch = {setSearch}
+      />
       <Content
         // Drilling props
-        items = {items}
+        // Two brackets are used since we wouldn't want the compiler to get confused about item.item being part of tthe object.
+        // The actual item and the value in the search bar (default state / search state) are converted to lowercase
+        // so that they match   
+        items = {items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck = {handleCheck}
         handleDelete = {handleDelete}
       />
