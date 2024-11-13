4b8fcbe7b4d0a1e60e28b814f4e5550c52ae3d4a
Saswat Levin <saswatlevin@gmail.com>
2024-11-13 22:54:30 +0530

    Added the AddItem form <<Chapter 9 | 01tut>>

---
+++ src/AddItem.js
@@ -0,0 +1,38 @@
+import { FaPlus } from 'react-icons/fa';
+import { useRef } from 'react';
+
+const addItem = ({newItem, setNewItem, handleSubmit}) => {
+  const inputRef = useRef();
+
+  return (
+    <form classname = 'addForm' onSubmit = {(submissionEvent) => handleSubmit(submissionEvent)}>
+        <label htmlFor = 'addItem'>Add Item</label>
+        {/* This is a controlled input */}
+        <input
+            autoFocus
+            ref = {inputRef}
+            id = 'addItem'
+            type = 'text'
+            placeholder = 'Add Item'
+            /* Required field */
+            required
+            /* Default state */
+            value = {newItem}
+            /* Set the new state for the new item */
+            onChange = { (submissionEvent) => setNewItem(submissionEvent.target.value)}
+
+        />
+        <button
+            type='submit'
+            aria-label = 'Add Item'
+            // To move the focus from the button to add item form just after clicking the button.
+            onClick = {() => inputRef.current.focus()}
+        >
+            <FaPlus/>
+        </button>
+
+    </form>
+  );
+};
+
+export default addItem;
